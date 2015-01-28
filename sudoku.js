e93ceb3df7624cae3cef301c8e2cc4787d97b52c
Tom May <tom@tommay.net>
2015-01-27 21:05:20 -0800

    Fix regexp that deletes comments in the puzzle file.

--- sudoku.js
+++ sudoku.js
@@ -4,7 +4,7 @@ var fs = require("fs");
 
 function main() {
     var text = fs.readFileSync(process.argv[2], {encoding: "utf8"});
-    text = text.replace(/#./g, "").replace(/\s/g, "");
+    text = text.replace(/#.*/g, "").replace(/\s/g, "");
     var solutions = new Puzzle(text).solve();
     console.log(solutions.length + " solutions");
 };
